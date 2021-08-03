d4458cad0b891acf325ecea6777ba574be00a512
9mtd <aduyend@gmail.com>
2021-08-02 20:30:52 -0500

    Commit

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SchoolPractice
 {
@@ -7,6 +9,38 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            var studentInstance = new Student
+            {
+                Name = "Amy",
+                StudentId = 1,
+                NumberOfCredits = 1,
+                Gpa = 4.0
+            };
+
+            var courseInstance = new Course
+            {
+                //CourseInfo = new Dictionary<int, string> 
+                //{ 
+                //    [123] = "C#"
+                //},
+                Students = new List<Student> { studentInstance },
+                CourseDescription = "C# Web Development covers building applications with ASP.NET. This module also covers object-oriented principles.",
+                Location = "online"
+            };
+
+            var teacherInstance = new Teacher
+            {
+                FirstName = "Scott",
+                LastName = "Kuhl",
+                Subject = "C#",
+                YearsTeaching = 10,
+            };
+
+            studentInstance.Print();
+            courseInstance.Print();
+            teacherInstance.Print();
+
+
         }
     }
 }
--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 // Start working here with your Student class.
 // To instantiate the Student class, add your code to the Main method in Program
@@ -7,9 +8,42 @@ namespace SchoolPractice
 {
     public class Student
     {
-        private string Name;
-        private int StudentId;
-        private int NumberOfCredits;
-        private double Gpa;
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public int NumberOfCredits { get; set; }
+        public double Gpa { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"***Student Info***\n Name: {Name}\n Student ID: {StudentId}\n Number of Credits: {NumberOfCredits}\n GPA: {Gpa}\n");
+        }
+    }
+
+    public class Course
+    {
+        //public Dictionary<int, string> CourseInfo = new Dictionary<int, string>();
+        public List<Student> Students { get; set; }
+        public string CourseDescription;
+        public string Location;
+
+        public void Print()
+        {
+            Console.WriteLine($"***Course Info***\n Student info: {Students}\n Course Description: {CourseDescription}\n Location: {Location}\n");
+        }
+    }
+
+
+
+    public class Teacher
+    {
+        public string FirstName;
+        public string LastName;
+        public string Subject;
+        public int YearsTeaching;
+
+        public void Print()
+        {
+            Console.WriteLine($"***Teacher Info***\n First Name: { FirstName}\n Last Name: {LastName}\n Subject: {Subject}\n Years Teaching: {YearsTeaching}");
+        }
     }
 }
